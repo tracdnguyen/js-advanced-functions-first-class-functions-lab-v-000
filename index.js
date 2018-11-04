32cbc87e649143b674cc0ffbf1dc9637547fef9a
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-04 22:20:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const returnFirstTwoDrivers = function(drivers){
   return drivers.slice(0, 2);
 }
+
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(drivers.length - 1, drivers.length - 3);
+}
